13781901ee22ce8135bbac32f69bc19abf68b8f9
Matt Lea <mlea@schematical.com>
2014-07-25 09:30:46 -0500

    kicking it off

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    app_dir: __dirname,
+    port:3030,
+    aws:{
+        accessKeyId:'AKIAJ5XOKQGKO7LLZY4A',
+        secretAccessKey:'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',
+        bucket_name:'byobob',
+        associateId:'holiday_helper-20'
+
+    },
+    search:{
+        products:['cheer bow cheerleading']
+    }
+}
---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module(
+        'iraas',
+    [
+        'ngRoute',
+        'ngCookies',
+        
+        'iraas.filters',
+        'iraas.directives'
+    ]
+).config(
+        [
+            '$routeProvider',
+            function($routeProvider) {
+
+                $routeProvider.when('/', { templateUrl: 'partials/welcome.html', controller: 'WelcomeCtl'});
+                $routeProvider.when('/home', { templateUrl: 'partials/home.html', controller: 'HomeCtl'});
+                $routeProvider.when('/draw', { templateUrl: 'partials/draw.html', controller: 'DrawCtl'});
+                $routeProvider.when('/suggest', { templateUrl: 'partials/suggest.html', controller: 'SuggestCtl'});
+                $routeProvider.when('/display', { templateUrl: 'partials/display.html', controller: 'DisplayCtl'});
+                $routeProvider.otherwise({redirectTo: '/'});
+        }
+    ]
+);
